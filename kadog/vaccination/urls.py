e24c6cb849eaea8a40ffa08b2517d253199467bd
Kurt Zidane Toledo <kurtzidanetoledo@gmail.com>
2024-02-04 14:23:33 +0800

    cleaned and fixed bugs in vaccine model

--- kadog/vaccination/urls.py
+++ kadog/vaccination/urls.py
@@ -1,14 +1,10 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
-from .views import VaccineUserRelationshipCreateView
+from django.urls import path
+from .views import VaccineUserRelationshipCreateView, VaccineListView
 
-router = routers.DefaultRouter()
-router.register(r'vaccines', views.VaccineViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('vaccines/join', VaccineUserRelationshipCreateView.as_view())
+    path('', VaccineListView.as_view()),
+    path('participate/', VaccineUserRelationshipCreateView.as_view())
 ]
