94769e332d0a2a2d0aadd076c0ba676272225a7f
Paul Ryan Gonzales <Yongzky15@gmail.com>
2024-11-08 23:39:40 +0800

    Update: Major changes for dogs directory.

--- kadog/dogs/urls.py
+++ kadog/dogs/urls.py
@@ -1,11 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import DogListCreateAPIView
-
+from .views import DogListCreateAPIView, DogDetailView, DogUpdateAPIView, DogRetrieveDestroyAPIView
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     # path('',),
     path('', DogListCreateAPIView.as_view()),
+    path('detail/<int:pk>/', DogDetailView.as_view()),
+    path('detail/<int:pk>/change/', DogUpdateAPIView.as_view()),
+    path('detail/<int:pk>/delete/', DogRetrieveDestroyAPIView.as_view()),
 ]
